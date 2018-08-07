e57214d91afc3f7285773511dd5d221c4c469783
jedi225 <jedtk@uci.edu>
2018-08-06 22:07:03 -0700

    Search results now push to DOM

--- assets/javascript/walmartApp.js
+++ assets/javascript/walmartApp.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $("#searchButton").on("click", function (event) {
         event.preventDefault();
         product = $("#productInput").val().trim();
+        var infoArea = $("#productInfo");
 
         var product;
         var queryURL = "http://api.walmartlabs.com/v1/search?apiKey=fpa5mauqm95qpzwweykc47uv&query=" + product;
@@ -13,11 +14,25 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
             var results = response.items;
+            infoArea.empty()
+            var productDiv = $("<div>");
 
             for (var i = 0; i < 1; i++) {
 
-            alert(results[i].name);
-            alert("$" + results[i].salePrice);
+                // alert(results[i].name);
+                // alert("$" + results[i].salePrice);
+
+                var image = $("<img>");
+                image.attr("src", results[i].mediumImage);
+                productDiv.append(image);
+
+                productDiv.append("<br>" + "<br>" + results[i].name);
+                productDiv.append("<br>" + "<br>" + "$" + results[i].salePrice);
+                productDiv.append("<br>" + "<br>" + results[i].shortDescription);
+                productDiv.append("<br>" + "<br>" + "Stock: " + results[i].stock);
+
+                infoArea.append(productDiv);
+
             }
 
         })
